bca4c3196dbbe8ff1402ef1d809765acaec9c375
Yamit <jpabon369@yahoo.com>
2022-04-19 21:58:46 +0300

    first commit

---
+++ dbHelpers.js
@@ -0,0 +1,58 @@
+const knex = require('knex');
+const config = require('./knexfile')
+const db = knex(config.development);
+
+
+
+//USER
+//function for creating the user 
+async function addUser(user){
+ await db('users').insert(user)  
+  return db('users').where({username:user.username})//way to get back the info on the registration process
+} // async and await work together since this is in order that program will wait until this process is over before moving. 
+
+//for every end point we need to creat a user
+function getAllUsers(){
+    return db('users')
+}
+
+function finUserByUserName (username){
+    return db('users').where({username:username}).first();
+}
+
+//function of getting user by id
+function findUserById (id){
+    return db('users').where({id:id}).first();
+}
+
+//function for removing user
+function removeUser (id){
+    return db('users')
+    .where({id:id})
+    .del()
+}
+
+//DESTINATION FUNCTIONS
+function getAllDestinations () {
+    return db('destinations')
+}
+
+async function addDestination (newDestination, user_id) {
+    await db('destinations')
+    .where({user_id:user_id})
+    .insert(newDestination)
+    // return db('destinations')
+}
+
+
+
+module.exports ={
+    addUser,
+    getAllUsers,
+    finUserByUserName,
+    findUserById,
+    removeUser,
+    getAllDestinations,
+    addDestination
+
+}
---
+++ migrations/20220415135512_create-users-table.js
@@ -0,0 +1,27 @@
+
+exports.up = function(knex) {
+  
+
+    return knex.schema.createTable('users', tbl=>{
+        tbl.increments()// for id of the parent column 
+        tbl.text('username',120).notNullable().unique().index()
+        tbl.text('password',200).notNullable()
+        tbl.text('imgUrl').notNullable()
+        tbl.timestamps(true,true)
+    }) //all of these is the table
+    .createTable('destinations', tbl=>{
+        tbl.increments()
+        tbl.text('title',200).notNullable().index()
+        tbl.text('description').notNullable()
+        tbl.text('imgUrl').notNullable()
+        tbl.timestamps(true,true)
+        tbl.integer('user_id').notNullable().unsigned().references('id').inTable('users').onDelete('CASCADE').onUpdate('CASCADE')
+    })
+
+
+
+};
+
+exports.down = function(knex) {
+  return knex.schema.dropTableIfExist('users').dropTableIfExist('destinations')
+};
