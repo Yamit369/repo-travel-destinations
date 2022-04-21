9b2633f448b41cb35c9094b376ed5790c0821dda
Yamit <jpabon369@yahoo.com>
2022-04-22 00:13:13 +0300

    solved the problem with the data base

--- migrations/20220415135512_create-users-table.js
+++ migrations/20220415135512_create-users-table.js
@@ -2,18 +2,18 @@
 exports.up = function(knex) {
   
 
-    return knex.schema.createTable('users', tbl=>{
+    return knex.schema.createTable('users',tbl=>{
         tbl.increments()// for id of the parent column 
         tbl.text('username',120).notNullable().unique().index()
         tbl.text('password',200).notNullable()
         tbl.text('imgUrl').notNullable()
         tbl.timestamps(true,true)
     }) //all of these is the table
-    .createTable('destinations', tbl=>{
+    .createTable('destinations',tbl=>{
         tbl.increments()
-        tbl.text('title',200).notNullable().index()
-        tbl.text('description',700).notNullable()
-        tbl.text('imgUrl').notNullable()
+        tbl.text('title').notNullable().index()
+        tbl.text('description').notNullable()
+        // tbl.text('imgUrl').notNullable()
         tbl.timestamps(true,true)
         tbl.integer('user_id').notNullable().unsigned().references('id').inTable('users').onDelete('CASCADE').onUpdate('CASCADE')
     })
@@ -23,5 +23,5 @@ exports.up = function(knex) {
 };
 
 exports.down = function(knex) {
-  return knex.schema.dropTableIfExist('users').dropTableIfExist('destinations')
+  return knex.schema.dropTableIfExist("users").dropTableIfExist("destinations")
 };
